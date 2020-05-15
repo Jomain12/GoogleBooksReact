8d58cd56bd9ee5f32600ce81efd97fb47092e4f5
Jomain Rouser <jomainrouser12@gmail.com>
2020-05-14 20:25:52 -0500

    hope

---
+++ client/build/precache-manifest.adf15d4e345532eec6add707ca7ff378.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ecc24914a22cd0b3bc5e",
+    "url": "/static/css/main.94d334b1.chunk.css"
+  },
+  {
+    "revision": "ecc24914a22cd0b3bc5e",
+    "url": "/static/js/main.c4b84af4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a4e09a2e807c0e01c192",
+    "url": "/static/js/2.23ca37b0.chunk.js"
+  },
+  {
+    "revision": "30c510591371193a3f25bce1975f0c97",
+    "url": "/index.html"
+  }
+];
