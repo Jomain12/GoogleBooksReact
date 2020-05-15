a0c3ec83edf31d2097edeb2ff10efd88d2b03b72
Jomain Rouser <jomainrouser12@gmail.com>
2020-05-14 21:07:50 -0500

    shoot me

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./app.css";
 import BookSearch from "./components/BookSearch/BookSearch";
 import SavedBooks from "./components/SavedBooks/SavedBooks";
-import NAV from "./components/Nav/nav";
+import NAV from "./components/Nav/Nav";
 
 function App() {
   return (
